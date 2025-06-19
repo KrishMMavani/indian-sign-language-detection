58594dac76d14b7793a4a6e2befce72f1fa548f7
Krish Mavani <120734012+KrishMMavani@users.noreply.github.com>
2025-06-19 22:07:20 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react',] 
-   },
-  base: './', 
+  base: './', // ✅ THIS LINE IS IMPORTANT for correct asset paths on Vercel
 });
